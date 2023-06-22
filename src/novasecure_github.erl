7dc20f59b48f319b126611d9266519aff8779f13
Daniel Widgren <daniel.widgren@gmail.com>
2023-06-23 01:10:29 +0200

    fixed callback function

--- src/novasecure_github.erl
+++ src/novasecure_github.erl
@@ -2,12 +2,15 @@
 
 -export([callback/1]).
 
-callback(Req) ->
+callback(#{parsed_qs := #{<<"code">> := Code}}) ->
     {ok, Confs} = application:get_env(novasecure, github),
-    Json = thoas:encode(Confs#{code => session_code, accept => json}),
+    Json = thoas:encode(Confs#{code => Code, redirect_url => <<"http://localhost:8080/user">>}),
     case shttpc:post(<<"https://github.com/login/oauth/access_token">>, Json, #{close => true,
-                                                                                headers => #{}}) of
+                                                                                headers => #{'Accept' => <<"application/json">>}}) of
         #{status := {201, _}, body := Body} -> 
             {ok, DecodedBody} = thoas:decode(Body),
+            logger:debug(DecodedBody);
+        Error -> logger:error(Error)
+    end.
 
             
