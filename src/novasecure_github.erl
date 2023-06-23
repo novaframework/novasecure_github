3a05afe5294fef76e22d1c005301a9ffeb95987b
Daniel Widgren <daniel.widgren@gmail.com>
2023-06-23 02:05:40 +0200

    now with a succseful flow

--- src/novasecure_github.erl
+++ src/novasecure_github.erl
@@ -3,11 +3,12 @@
 -export([callback/1]).
 
 callback(#{parsed_qs := #{<<"code">> := Code}}) ->
-    {ok, Confs} = application:get_env(novasecure, github),
-    Json = thoas:encode(Confs#{code => Code, redirect_url => <<"http://localhost:8080/user">>}),
-    case shttpc:post(<<"https://github.com/login/oauth/access_token">>, Json, #{close => true,
-                                                                                headers => #{'Accept' => <<"application/json">>}}) of
-        #{status := {201, _}, body := Body} -> 
+    {ok, #{client_id := ClientId,
+           client_secret := ClientSecret}} = application:get_env(novasecure, github),
+    Params = <<"client_id=", ClientId/binary, "&client_secret=", ClientSecret/binary, "&code=", Code/binary, "&redirect_url=http://localhost:8080/user">>,
+    case shttpc:post(<<"https://github.com/login/oauth/access_token?", Params/binary>>, <<>>, #{close => true,
+                                                                                                headers => #{'Accept' => <<"application/json">>}}) of
+        #{status := {200, _}, body := Body} -> 
             {ok, DecodedBody} = thoas:decode(Body),
             logger:debug(DecodedBody);
         Error -> logger:error(Error)
